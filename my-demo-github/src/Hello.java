c0e64a51aade1be7b04d01e334eb812ce9535924
Vaishnavi Rahate <vaishnavirahate02@gmail.com>
2021-03-23 17:46:51 +0530

    modified the method1() in hello.java

--- my-demo-github/src/Hello.java
+++ my-demo-github/src/Hello.java
@@ -2,7 +2,8 @@
 public class Hello {
 
 	public void method1() {
-		System.out.println("Hello Everyone");
-		System.out.println("How's all?");
+		System.out.println("line 1");
+		System.out.println(" line 2");
+		System.out.println(" line 3");
 	}
 }
