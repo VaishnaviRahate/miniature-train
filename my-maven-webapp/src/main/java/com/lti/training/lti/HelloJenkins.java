e95e665bda4b67c08c85214a6807b3f44171554c
Vaishnavi Rahate <vaishnavirahate02@gmail.com>
2021-03-26 11:11:39 +0530

    New project added named as my-maven-webapp

---
+++ my-maven-webapp/src/main/java/com/lti/training/lti/HelloJenkins.java
@@ -0,0 +1,29 @@
+package com.lti.training.lti;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class HelloJenkins extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.write("<html><body>");
+		out.write("<h1>Hello Everyone</h1>");
+		out.write("<h2>" + LocalDateTime.now() + "</h2>");
+		out.write("</body></html>");
+	}
+
+
+
+}
